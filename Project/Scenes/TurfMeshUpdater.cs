7c5b7f4b621be167604796fc1a37d7e2b4336432
Willburd <7099514+Willburd@users.noreply.github.com>
2024-03-04 05:15:36 -0500

    turf meshes started

--- Project/Scenes/TurfMeshUpdater.cs
+++ Project/Scenes/TurfMeshUpdater.cs
@@ -3,12 +3,25 @@ using System;
 using System.Collections.Generic;
 
 [GlobalClass]
-public partial class TurfMeshUpdater : MeshInstance3D
+public partial class TurfMeshUpdater : Node3D
 {
 
     public static Dictionary<string,Resource> texture_cache = new Dictionary<string,Resource>();
 
     private string last_tex = "";
+
+    private MeshInstance3D[] mesh_array;
+
+    public override void _Ready()
+    {
+        mesh_array = new MeshInstance3D[ChunkController.chunk_size * ChunkController.chunk_size];
+        int i = 0;
+        foreach(MeshInstance3D mesh in GetChildren())
+        {
+            mesh_array[i] = mesh;
+            i++;
+        }
+    }
 
     public void MeshUpdated(NetworkChunk turf)
     {
